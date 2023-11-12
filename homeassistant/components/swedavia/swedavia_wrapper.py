fb3d0aa2b4f44ec484dce0ab91a31d29198ca946
decltype(auto) <24841307+AeroStun@users.noreply.github.com>
2023-11-12 20:34:47 +0100

    Fill async_get_wait_time

--- homeassistant/components/swedavia/swedavia_wrapper.py
+++ homeassistant/components/swedavia/swedavia_wrapper.py
@@ -32,14 +32,14 @@ class SwedaviaWrapper:
         self, airport: str, flight_number: str, date: str
     ) -> FlightInfo:
         base_url = "https://api.swedavia.se/flightinfo/v2/query"
-
-        filter_params = f"airport eq '{airport}' and scheduled eq '{scheduled}' and flightType eq '{flight_type}' and flightId eq '{flight_id}'"
+        flight_type = 'D'
+        filter_params = f"airport eq '{airport}' and scheduled eq '{scheduled}' and flightType eq '{flight_type}' and flightId eq '{flight_number}'"
         url = f"{base_url}?filter={filter_params}"
     
         headers = {
             "Accept": "application/json",
             "Cache-Control": "no-cache",
-            "Ocp-Apim-Subscription-Key": self.flight_info_api_key = flight_info_api_key,
+            "Ocp-Apim-Subscription-Key": self.flight_info_api_key,
         }
     
         async with client_session.get(url, headers=headers) as response:
@@ -54,8 +54,21 @@ class SwedaviaWrapper:
 
     async def async_get_wait_time(
         self, airport: str, flight_number: str, date: str
-    ) -> WaitTime:
-        pass
+    ) -> [WaitTime]:
+        url = f"https://api.swedavia.se/waittimepublic/v2/airports/{airport}/flights?flightid={flight_number}&date={date}"
+
+        headers = {
+            'Accept': 'application/json',
+            'Cache-Control': 'no-cache',
+            'Ocp-Apim-Subscription-Key': self.wait_time_api_key,
+        }
+
+        async with client_session.get(url, headers=headers) as response:
+            if response.status == 200:
+                data = await response.json()
+                return [WaitTime.from_dict(wait_time) for wait_time in data.get("waitTimes")]
+            else:
+                raise Exception(f"Failed to fetch wait times. Status code: {response.status}")
 
 
 class InvalidWaitTimeKey(Exception):

cf1e34f1beb4ed760a5b6447ebffa63d851e4af4
decltype(auto) <24841307+AeroStun@users.noreply.github.com>
2023-11-12 20:25:14 +0100

    Add fields to SwedaviaWrapper

--- homeassistant/components/swedavia/swedavia_wrapper.py
+++ homeassistant/components/swedavia/swedavia_wrapper.py
@@ -8,18 +8,25 @@ from .flight_data import Flight, WaitTime
 class SwedaviaWrapper:
     """Class used to communicate with Swedavia's api."""
 
+    client_session: aiohttp.ClientSession
+    flight_info_api_key: str
+    wait_time_api_key: str
+
+    
     def __init__(
         self,
         client_session: aiohttp.ClientSession,
         flight_info_api_key: str,
         wait_time_api_key: str,
     ) -> None:
-        pass
+        self.client_session = client_session
+        self.flight_info_api_key = flight_info_api_key
+        self.wait_time_api_key = wait_time_api_key
 
     async def async_get_client_session(
         self, hass: HomeAssistant, verify_ssl=True
     ) -> aiohttp.ClientSession:
-        pass
+        return self.client_session
 
     async def async_get_flight_info(
         self, airport: str, flight_number: str, date: str
